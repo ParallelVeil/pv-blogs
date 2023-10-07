85d3e992833b6d0e6b94d9d47bc2f49253b8ef5e
ishiko732 <ishiko732@gmail.com>
2023-10-08 00:05:05 +0800

    Feat/img support for CustomImage

--- app/components/CustomImage.tsx
+++ app/components/CustomImage.tsx
@@ -6,12 +6,15 @@ type Props = {
     priority?: string,
 }
 
-export default function CustomImage({ src, alt, priority }: Props) {
+export default function CustomImage({ src, alt, priority }: any) {
     if (src.startsWith("images/") ) {
         src=`http://localhost:3000/api/${src}`
     }
     if (src.startsWith("/images/")) {
         src=`http://localhost:3000/api${src}`
+    }
+    if(src.startsWith("./images/")){
+        src=`http://localhost:3000/api${src.substring(1)}`
     }
     return (
         <div className="w-full h-full">
