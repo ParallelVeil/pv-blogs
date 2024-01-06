1ad0e1ddc18524fc1f23487246bc47cf26e52a3d
ishiko732 <ishiko732@gmail.com>
2024-01-06 23:46:12 +0800

    bump dependencies

--- lib/getFormattedDate.ts
+++ lib/getFormattedDate.ts
@@ -1,3 +1,11 @@
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+
 export default function getFormattedDate(dateString: string): string {
-    return new Intl.DateTimeFormat('zh-CN', { dateStyle: 'long' }).format(new Date(dateString))
+    dayjs.extend(utc) // use plugin
+    dayjs.extend(timezone) // use plugin
+    dayjs.extend(localizedFormat)  // use plugin
+    return dayjs.utc(dateString).tz(dayjs.tz.guess()).format('LLL')
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,7 @@ const config: Config = {
     },
   },
   plugins: [
+    require("daisyui"),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
   ],
