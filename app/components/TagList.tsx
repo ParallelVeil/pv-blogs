5da0c331c797d8d0a660eba0bd6e58599ecbc7a9
ishiko732 <ishiko732@gmail.com>
2024-01-07 16:30:33 +0800

    Fix/dropdown

--- app/components/TagList.tsx
+++ app/components/TagList.tsx
@@ -4,13 +4,18 @@ import { useEffect } from "react";
 
 export default function TagList({ tags }: { tags: string[] }) {
   useEffect(() => {
-    window.addEventListener("click", function (e: MouseEvent) {
-      document.querySelectorAll(".dropdown").forEach(function (dropdown: Element) {
-        if (e.target&&!dropdown.contains(e.target as Node)) {
-          (dropdown as HTMLDetailsElement).open = false;
-        }
-      });
-    });
+    const dropdown_handler = function (e: MouseEvent) {
+      document
+        .querySelectorAll(".dropdown")
+        .forEach(function (dropdown: Element) {
+          if (e.target && !dropdown.contains(e.target as Node)) {
+            (dropdown as HTMLDetailsElement).open = false;
+          }
+        });
+    };
+
+    window.addEventListener("click", dropdown_handler);
+    return () => window.removeEventListener("click", dropdown_handler);
   }, []);
   return tags.map((tag) => (
     <li key={tag}>
