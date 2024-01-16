e26edad8c829005c532108876b65e995fcb4cf8c
ishiko732 <ishiko732@gmail.com>
2024-01-16 23:40:05 +0800

    add Loading

--- app/about/components/AuthorImage.tsx
+++ app/about/components/AuthorImage.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+function Loading(){
+    return <div className="skeleton w-[200] h-[200] rounded-full shrink-0"></div>
+}
 export default function AuthorImage() {
-    return (
+  return (
+    <Suspense fallback={<Loading/>}>
       <Image
         className="border-4 border-[#212936] dark:border-slate-500 drop-shadow-xl shadow-[#212936] rounded-full mx-auto mt-8 z-0"
         src="/images/ishiko.png"
@@ -10,10 +16,10 @@ export default function AuthorImage() {
         alt="ishiko"
         priority={true}
       />
-    );
-  }
+    </Suspense>
+  );
+}
 
-
-export const DynamicAuthorImage = dynamic(() => import('./AuthorImage'), {
-    loading: () => <div className="skeleton w-[200] h-[200] rounded-full shrink-0"></div>,
-})
+export const DynamicAuthorImage = dynamic(() => import("./AuthorImage"), {
+  loading: () => <Loading/>,
+});
